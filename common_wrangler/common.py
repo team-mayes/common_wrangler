070f177619edf4a7e7d184a9fd9e6f6b4089f8d0
Heather Mayes <hmayes@hmayes.com>
2019-10-03 19:17:46 -0600

    moved calc_dist to common

--- common_wrangler/common.py
+++ common_wrangler/common.py
@@ -224,6 +224,10 @@ def calc_k(temp, delta_gibbs):
     return BOLTZ_CONST * temp / PLANCK_CONST * math.exp(-delta_gibbs / (RG * temp))
 
 
+def calc_dist(a, b):
+    return np.linalg.norm(np.subtract(a, b))
+
+
 def pbc_dist(a, b, box):
     # TODO: make a test that ensures the distance calculated is <= sqrt(sqrt((a/2)^2+(b/2)^2) + (c/2)^2)) ?
     return np.linalg.norm(pbc_calc_vector(a, b, box))
